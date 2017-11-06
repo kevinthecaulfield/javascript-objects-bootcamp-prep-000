fc836f35ff195ea5f3b75a04c42c095fae3552a2
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-06 23:39:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,4 +9,4 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
   return playlist
 }
-const playlist = {Kanye: "Gold Digger"}
+var playlist = {Kanye: "Gold Digger"}
