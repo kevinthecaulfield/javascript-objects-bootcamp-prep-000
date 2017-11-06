f919916fd5d924d2ffd8f49777430783294c295d
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-06 23:47:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, 'artistName') {
-  return delete playlist.artistName
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
 }
